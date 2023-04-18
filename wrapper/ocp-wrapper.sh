53aac49d40bb5fec66598131d7d999c8746da1eb
Mitesh Sharma <mitsharm@redhat.com>
2023-04-18 14:02:37 +0530

    update

--- wrapper/ocp-wrapper.sh
+++ wrapper/ocp-wrapper.sh
@@ -1,2 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Author: 
+#   - Mitesh The Mouse, mitsharm@redhat.com
+#   - Tyrell Reddy, treddy@redhat.com
+#   - Prakhar Srivastava, psrivast@redhat.com
+# Team: APAC-DemoLISHERS
+# License: GPLv3
+# Version: 0.1
 
